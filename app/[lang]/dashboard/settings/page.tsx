76ecfa610d5e200264933c1128bfa3fcd2335dba
Shanto93 <shantoislam7363@gmail.com>
2026-01-30 23:55:01 +0600

    Image optimization

--- app/[lang]/dashboard/settings/page.tsx
+++ app/[lang]/dashboard/settings/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import myImg from "@/public/assets/my-photo.jpg";
 const SettingsPage = () => {
+
   return (
     <div>
       <h2>This is settings Page</h2>
+      <div className="w-96 border-2  border-teal-300 relative">
+        <Image className="object-cover" fill src={myImg} alt="My Photo"></Image>
+      </div>
     </div>
   );
 };
